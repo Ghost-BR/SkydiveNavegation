c09dac90c2876262284a43e5a1b3210d8204d955
Christofer Bertonha <christoferbertonha@gmail.com>
2013-05-03 18:41:49 -0300

    Layout almost finished

--- SkydiveNavegation/main.py
+++ SkydiveNavegation/main.py
@@ -1,25 +1,44 @@
 '''
 Created on May 1, 2013
 
-@author: christofer
+@author: Christofer Bertonha
 '''
+
 
 import kivy
 kivy.require('1.6.0')
 
+import random
+
 from kivy.app import App
 from kivy.graphics import Color, Ellipse, Line
-from kivy.properties import ListProperty
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.button import Button
+from kivy.properties import ListProperty, ObjectProperty
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.image import Image
 
 
-class RootWidget(BoxLayout):
+class RootWidget(GridLayout):
+    map = ObjectProperty(None)
+    wind = ObjectProperty(None)
+    wind_button = ObjectProperty(None)
+    text_area = ObjectProperty(None)
+
+    def random_wind(self):
+        angle = random.randint(0, 359)
+        self.wind.rotation = angle
+
+    def change_wind(self, *args):
+        if len(self.map.points) > 4:
+            self.map.clear_points()
+            self.random_wind()
+
+
+class LocationPlace(Image):
 
     points = ListProperty()
 
     def on_touch_down(self, touch):
-        if len(self.points) < 4:
+        if self.collide_point(*touch.pos):
             with self.canvas.after:
                 Color(1, 1, 0)
                 d = 15.
@@ -28,22 +47,21 @@ class RootWidget(BoxLayout):
                 if len(self.points):
                     line.points += self.points[-1]
                 self.points.append(touch.pos)
-        else:
-            self.canvas_clear()
-        return True
+            return True
+        return super(LocationPlace, self).on_touch_down(touch)
 
-    def canvas_clear(self):
+    def clear_points(self):
         self.points = []
         self.canvas.after.clear()
 
 
-class WindButton(Button):
-    pass
-
-
 class MyApp(App):
     def build(self):
-        return RootWidget()
+        root = RootWidget()
+        root.map.bind(points=root.change_wind)
+        root.wind_button.bind(on_press=root.change_wind)
+        root.random_wind()
+        return root
 
 
 if __name__ in ('__main__', '__android__'):
